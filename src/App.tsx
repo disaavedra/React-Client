58d99884f5802f8966d21b802a682b7d68bedf51
disaavedra <62458438+disaavedra@users.noreply.github.com>
2023-06-12 11:36:10 -0400

    please

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainView from './pages/MainView';
 import TestView from './pages/TestView';
-import { Navigation } from "./components/Navigation"
+// import { Navigation } from "./components/Navigation"
 
 function App() {
   return (
     <BrowserRouter>
-        <Navigation/>
+        {/* <Navigation/> */}
         <Routes>
             <Route path="/:id_evaluacion/:id_usuario" element={<MainView/>} />
             <Route path="/:id_evaluacion/:id_usuario/test" element={<TestView/>} />
