ae6dd1b2b4855f50cfe7effcb2df97bfbb4f541e
disaavedra <62458438+disaavedra@users.noreply.github.com>
2023-06-12 08:15:33 -0400

    React-client V1

--- src/api/evaluations.api.tsx
+++ src/api/evaluations.api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const djangoApi = axios.create({
-  baseURL: 'https://web-django-api.herokuapp.com/api/v1/'
+  baseURL: 'https://web-django-api.herokuapp.com/api/v1'
 });
 
 export const getEvaluation = (id: any) => {
